4a4a8a20122a2bb892382d451f517fd39c64af8d
Jade Huynh <35jadeh@gmail.com>
2019-08-08 14:18:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,12 @@
 class Dog
-  def initialize(dog_name)
+  def initialize(dog_name, dog_breed = "mutt")
     @name = dog_name
+    @breed = dog_breed
   end
   def name
     @name 
   end
+  def breed
   def name=(new_name)
     @name = new_name
   end
